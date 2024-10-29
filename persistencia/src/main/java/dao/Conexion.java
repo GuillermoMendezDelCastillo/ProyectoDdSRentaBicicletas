c8b0e8d235f39e1d2d0aefd30cefc0c01f53a8e2
GuillermoMendezDelCastillo <guillermo.mendez215742@potros.itson.edu.mx>
2024-10-28 19:51:15 -0700

    Prueba conexion a BD
    Prueba de conexion a Base de datos de MySQL. Se debe de llamar "renta_bicicletas", recordar cambiar la contraseña en el archivo persistence.xml

---
+++ persistencia/src/main/java/dao/Conexion.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+/**
+ *
+ * @author Gui26
+ */
+public class Conexion implements IConexion{
+    
+    @Override
+    public EntityManager crearConexion(){
+        EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("renta_bicicletas");
+        EntityManager entityManager = emFactory.createEntityManager();
+        
+        return entityManager;
+    }
+    
+}
